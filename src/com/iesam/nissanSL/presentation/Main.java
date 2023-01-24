4ac3dbe878ec60682df84b7972a2dd606d4559b6
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2023-01-24 14:31:18 +0100

    Parte del codigo sobre la clase Chasis y su implementacion en main

--- src/com/iesam/nissanSL/presentation/Main.java
+++ src/com/iesam/nissanSL/presentation/Main.java
@@ -1,5 +1,6 @@
 package com.iesam.nissanSL.presentation;
 
+import com.iesam.nissanSL.domain.models.Chasis;
 import com.iesam.nissanSL.domain.models.Motor;
 import com.iesam.nissanSL.domain.models.Rueda;
 import com.iesam.nissanSL.domain.models.CuadroMando;
@@ -23,8 +24,6 @@ public class Main {
         ruedaB.setDiametro("205");
         ruedaB.setUnidades(3);
 
-
-
         Motor motorA = new Motor();
         motorA.setCodigo("1M");
         motorA.setModelo("x6000");
@@ -39,8 +38,6 @@ public class Main {
         motorB.setCaballos("160cv");
         motorB.setUnidades(2);
 
-
-
         CuadroMando cuadroMandoA = new CuadroMando();
         cuadroMandoA.setCodigo("1C");
         cuadroMandoA.setModelo("LM2000");
@@ -50,6 +47,51 @@ public class Main {
         cuadroMandoB.setCodigo("2C");
         cuadroMandoB.setModelo("LM6000");
         cuadroMandoB.setUnidades(2);
+
+        Chasis chasisAAAA = new Chasis();
+        chasisAAAA.setCodigoBastidor("AAAA");
+        chasisAAAA.setMarca("Ford");
+        chasisAAAA.setModelo("Mondeo");
+        chasisAAAA.addAccesorio(ruedaA);
+        //Modificar el número de unidades de ruedas A (-1 porque entendemos que en el stock son paquetes)
+            // –> caso de uso
+        chasisAAAA.addAccesorio(motorB);
+        chasisAAAA.addAccesorio(cuadroMandoA);
+        //Caso de uso para guardar este chasis
+
+        Chasis chasisBBBB = new Chasis();
+        chasisBBBB.setCodigoBastidor("BBBB");
+        chasisBBBB.setMarca("Audi");
+        chasisBBBB.setModelo("A1");
+        chasisBBBB.addAccesorio(ruedaB);
+        //Modificar el número de unidades de ruedas B (-1 porque entendemos que en el stock son paquetes)
+            // –> caso de uso
+        chasisBBBB.addAccesorio(motorB);
+        chasisBBBB.addAccesorio(cuadroMandoB);
+        //Caso de uso para guardar este chasis
+
+        Chasis chasisCCCC = new Chasis();
+        chasisCCCC.setCodigoBastidor("CCCC");
+        chasisCCCC.setMarca("Land Rover");
+        chasisCCCC.setModelo("Defender");
+        chasisCCCC.addAccesorio(ruedaA);
+        //Modificar el número de unidades de ruedas A (-1 porque entendemos que en el stock son paquetes)
+            // –> caso de uso
+        chasisCCCC.addAccesorio(cuadroMandoA);
+        chasisCCCC.addAccesorio(motorA);
+        //Caso de uso para guardar este chasis
+
+        Chasis chasisDDDD = new Chasis();
+        chasisDDDD.setCodigoBastidor("DDDD");
+        chasisDDDD.setMarca("Citroen");
+        chasisDDDD.setModelo("Sara");
+        chasisDDDD.addAccesorio(ruedaA);
+        //Modificar el número de unidades de ruedas A (-1 porque entendemos que en el stock son paquetes)
+            // –> caso de uso
+        chasisDDDD.addAccesorio(motorA);
+        chasisDDDD.addAccesorio(cuadroMandoB);
+        //Caso de uso para guardar este chasis
+
 
     }
 }
